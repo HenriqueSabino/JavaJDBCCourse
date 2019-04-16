b11c714f0fdefd294978d880599c4cb915f41099
HenriqueSabino <h.sabinocosta@gmail.com>
2019-04-16 11:08:16 -0300

    Learning how to update rows

--- src/application/Program.java
+++ src/application/Program.java
@@ -8,44 +8,26 @@ import java.text.SimpleDateFormat;
 
 public class Program {
     public static void main(String[] args) {
-        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
         Connection connection = null;
         PreparedStatement preparedStatement = null;
 
         try{
             connection = DB.getConnection();
             preparedStatement = connection.prepareStatement(
-                        "insert into seller "
-                        + "(Name, Email, BirthDate, BaseSalary, DepartmentId) "
-                        + "values (?, ?, ?, ?, ?)",
-                        Statement.RETURN_GENERATED_KEYS);
+                    "update seller "
+                    + "set BaseSalary = BaseSalary + ? "
+                    + "where "
+                    + "(seller.DepartmentId = ?)"
+            );
 
-            preparedStatement.setString(1, "Carl Purple");
-            preparedStatement.setString(2, "carl@gmail.com");
-            preparedStatement.setDate(3, new java.sql.Date(sdf.parse("22/04/1985").getTime()));
-            preparedStatement.setDouble(4, 3000.0);
-            preparedStatement.setInt(5, 4);
+            preparedStatement.setDouble(1, 200.0);
+            preparedStatement.setInt(2, 2);
 
             int rowsAffected = preparedStatement.executeUpdate();
 
-            if (rowsAffected > 0){
-                ResultSet resultSet = preparedStatement.getGeneratedKeys();
-
-                while(resultSet.next()){
-                    int id = resultSet.getInt(1);
-                    System.out.println("Done! Id = " + id);
-                }
-
-                System.out.printf("Done! %d rows affected", rowsAffected);
-            }
-            else{
-                System.out.println("No rows affected");
-            }
+            System.out.printf("Done! %d row(s) affected", rowsAffected);
         }
         catch (SQLException e){
-            e.printStackTrace();
-        }
-        catch (ParseException e){
             e.printStackTrace();
         }
         finally {
